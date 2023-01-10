7bf509cb487aad93b14f7eb3809e39527750f0bb
K4-R4 <mabdman512@gmail.com>
2023-01-10 20:00:22 +0900

    Refactoring

--- file-converter.py
+++ file-converter.py
@@ -2,22 +2,21 @@ import markdown
 import sys
 
 def main():
-    if len(sys.argv) != 4:
-        print("入力が正しくありません")
-        sys.exit()
-    if sys.argv[1] == "markdown":
-        try:
-            inputpath = sys.argv[2]
-            outputpath = sys.argv[3]
-            with open(inputpath, "r") as f:
-                contents = f.read()
-            html = markdown.markdown(contents)
-            with open(outputpath, "w") as f:
-                f.write(html)
-        except FileNotFoundError:
-            print("入力ファイルが存在しません")
-    else:
-        print("無効なコマンドです")
+    try:
+        if sys.argv[1] == "markdown":
+                inputpath = sys.argv[2]
+                outputpath = sys.argv[3]
+                with open(inputpath, "r", encoding="utf-8") as f:
+                    contents = f.read()
+                html = markdown.markdown(contents)
+                with open(outputpath, "w", encoding="utf-8") as f:
+                    f.write(html)
+        else:
+            print("無効なコマンドです")
+    except FileNotFoundError:
+        print("入力ファイルが存在しません")
+    except IndexError:
+        print("引数の数が正しくありません")
 
 if __name__ == "__main__":
     main()
